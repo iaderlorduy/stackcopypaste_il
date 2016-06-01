1e1b617409c4a8d1ba99a29b63c106ca0fd6e229
iaderlorduy <iaderlor@hotmail.com>
2016-06-01 01:17:35 +0000

    Create basic authentication

--- Gemfile
+++ Gemfile
@@ -36,6 +36,10 @@ gem 'foundation-rails', '~> 5.5', '>= 5.5.3.2'
 
 gem 'paperclip', '~> 4.3', '>= 4.3.2'
 
+gem 'devise', '~> 4.1', '>= 4.1.0'
+
+gem 'letter_opener', '~> 1.4', '>= 1.4.1'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160601004257) do
+ActiveRecord::Schema.define(version: 20160601011145) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 20160601004257) do
     t.string   "photo_content_type"
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
