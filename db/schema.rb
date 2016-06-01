a20b4df52bd8604e62869bd932e45ed921b20650
iaderlorduy <iaderlor@hotmail.com>
2016-06-01 02:30:16 +0000

    solutions bugs and feedback

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160601020337) do
+ActiveRecord::Schema.define(version: 20160601022535) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -38,6 +38,11 @@ ActiveRecord::Schema.define(version: 20160601020337) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "username"
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "location"
+    t.text     "biography"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
