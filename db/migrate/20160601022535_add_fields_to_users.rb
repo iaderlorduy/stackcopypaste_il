5ae352aaef4d5bf8be6a41fbee7e192ed2c87117
iaderlorduy <iaderlor@gmail.com>
2016-06-06 22:29:34 -0500

    Migrations

--- db/migrate/20160601022535_add_fields_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddFieldsToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :username, :string
-    add_column :users, :firstname, :string
-    add_column :users, :lastname, :string
-    add_column :users, :location, :string
-    add_column :users, :biography, :text
-  end
-end
